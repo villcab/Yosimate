c7099d2a8ce20a7a2d2432f15d04a6c3d8d22ad5
Villca Soliz Bismarck <bismarck.villca@gmail.com>
2017-04-26 10:14:32 -0400

    Base projects with icons Yosimate OS

---
+++ install-icon-theme.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+clear
+echo '#------------------------------------------#'
+echo '#     Yosimate Icon Theme Install Script      #'
+echo '#------------------------------------------#'
+cp -R ../Yosemite /usr/share/icons
+chmod 777 -R /usr/share/icons/Yosemite/
+echo 'Success.'
